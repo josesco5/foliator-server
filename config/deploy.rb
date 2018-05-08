2852514f7c32e5e4bc745a228df83eefa0fb051a
José Escorche <josesco5@gmail.com>
2018-05-08 16:22:28 -0300

    Modify Mina's deploying settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
-require 'mina/rails'
+require 'mina/bundler'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (https://rvm.io)
+require 'mina/rvm'    # for rvm support. (https://rvm.io)
+require 'mina/puma'
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -10,23 +10,21 @@ require 'mina/git'
 #   branch       - Branch name to deploy. (needed by mina/git)
 
 set :application_name, 'foliator-app'
-# set :domain, 'foobar.com'
-set :domain, 'http://ec2-35-165-208-103.us-west-2.compute.amazonaws.com:3000'
-# set :deploy_to, '/var/www/foobar.com'
+set :domain, 'ec2-35-165-208-103.us-west-2.compute.amazonaws.com'
 set :deploy_to, '/home/ubuntu/deployment'
 set :repository, 'git@github.com:josesco5/foliator-server.git'
-set :branch, 'master'
+set :branch, 'feature/mina-deploy'
 
 # Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
+set :user, 'ubuntu'          # Username in the server to SSH to.
 #   set :port, '30000'           # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_dirs, fetch(:shared_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/uploads')
+set :shared_files, fetch(:shared_files, []).push('config/puma.rb')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
@@ -36,8 +34,7 @@ task :remote_environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
-  invoke :'rvm:use', 'ruby-2.4.1p111@default'
+  invoke :'rvm:use', 'ruby-2.4.1@default'
 end
 
 # Put any custom commands you need to run at setup
@@ -58,15 +55,11 @@ task :deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    # invoke :'rails:db_migrate'
-    # invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
-      in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
-      end
+      invoke :'puma:restart'
+      invoke :'puma:phased_restart'
     end
   end
 
