33346c86e0776eb8217a104ed2a6bc9b6ad608cc
José Escorche <josesco5@gmail.com>
2018-05-14 11:46:20 -0400

    Add local foliator gem

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'document-foliator', '0.1.0', :path => './local-gems/document-foliator-0.1.0'
+
 gem 'sinatra'
 gem 'puma'
--- foliator_app.rb
+++ foliator_app.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
+require 'document/foliator'
 
 class FoliatorApp < Sinatra::Application
   configure { set :server, :puma }
 
   get '/' do
+    puts Document::Foliator::VERSION
     'Hello World'
   end
 end
